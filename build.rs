2967ff98c5b16471d3f95f368a26f3497eaae2ab
White-Oak <lpzhelud@gmail.com>
2016-06-30 06:55:13 +0300

    Fixed trobles building crate

--- build.rs
+++ build.rs
@@ -10,6 +10,11 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let path = Path::new(&manifest_dir).join("DOtherSide").join("build").join("lib");
 
+    // use std::io::prelude::*;
+    // use std::fs::File;
+    //
+    // let mut f = File::create("foo.txt").unwrap();
+    // f.write_all(format!("{}", path.display()).as_bytes()).unwrap();
     println!("cargo:rustc-flags=-L {}", path.display());
     println!("cargo:rerun-if-changed={}", path.display());
     println!("cargo:rustc-link-lib=dylib=DOtherSide");
