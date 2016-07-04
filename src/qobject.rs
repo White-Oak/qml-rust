3740e97adb93a286c2d5f447235eff7fc8923a22
White-Oak <lpzhelud@gmail.com>
2016-07-04 03:17:37 +0300

    Added some definitions for future work on connecting signals and slots

--- src/qobject.rs
+++ src/qobject.rs
@@ -16,8 +16,34 @@ extern "C" {
                           metaObject: DosQMetaObject,
                           dObjectCallback: DObjectCallback)
                           -> DosQObject;
+    fn dos_qobject_signal_connect(senderVPtr: DosQObject,
+                                  signal: *const libc::c_char,
+                                  receiverVPtr: DosQObject,
+                                  method: *const libc::c_char,
+                                  qtype: i32)
+                                  -> bool;
 }
 
+macro_rules! QT_connect {
+    ($sender:ident, $signal:ident, $receiver:ident, $method:tt) => {{
+        unimplemented!()
+    }}
+}
+
+/// This enum describes the types of connection that can be used between signals and slots.
+/// In particular, it determines whether a particular signal is delivered to a slot immediately or queued for delivery at a later time.
+pub enum QtConnectionType {
+    /// **(Default)** If the receiver lives in the thread that emits the signal, Qt::DirectConnection is used. Otherwise, Qt::QueuedConnection is used. The connection type is determined when the signal is emitted.
+    AutoConnection = 0,
+    /// The slot is invoked immediately when the signal is emitted. The slot is executed in the signalling thread.
+    DirectConnection = 1,
+    /// The slot is invoked when control returns to the event loop of the receiver's thread. The slot is executed in the receiver's thread.
+    QueuedConnection = 2,
+    /// Same as Qt::QueuedConnection, except that the signalling thread blocks until the slot returns. This connection must not be used if the receiver lives in the signalling thread, or else the application will deadlock.
+    BlockingQueuedConnection = 3,
+    /// This is a flag that can be combined with any one of the above connection types, using a bitwise OR. When Qt::UniqueConnection is set, QObject::connect() will fail if the connection already exists (i.e. if the same signal is already connected to the same slot for the same pair of objects). This flag was introduced in Qt 4.6.
+    UniqueConnection = 0x80,
+}
 
 /// Called when a slot should be executed
 /// @param self The pointer to the QObject in the binded language
