f34e465625c0000385a088f245489f11adfc9288
White-Oak <lpzhelud@gmail.com>
2016-07-11 08:11:42 +0300

    Updated docs

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
+implementors["libc"] = [];implementors["qml"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='qml/struct.QVariant.html' title='qml::QVariant'>QVariant</a>",];implementors["qml"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='qml/struct.QVariant.html' title='qml::QVariant'>QVariant</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
